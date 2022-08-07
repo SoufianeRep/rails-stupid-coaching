a9ec80530e852a0839b7bde17b3360809c6e6365
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-07 23:17:36 +0900

    modified auestions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,15 @@
+# Questions Controller
 class QuestionsController < ApplicationController
-  def ask
-
-  end
+  def ask; end
 
   def answer
     @question = params[:question]
-    if @question.present?
-      @answer = if @question == 'I am going to work'
-                  'Great!'
-                elsif @question.end_with?('?')
-                  'Silly question, get dressed and go to work!'
-                else
-                  'I don\'t care, get dressed and go to work!'
-                end
-    end
+    @answer = if @question == 'I am going to work'
+                'Great!'
+              elsif @question.end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
   end
 end
